76f6ca08788b2c5f12758b185aecf1a258f55bb9
t1fan <artem.t1fan@gmail.com>
2021-04-17 17:18:31 +0300

    Доделал валидацию ввода юзера.

--- lesson2/lesson2_task3.py
+++ lesson2/lesson2_task3.py
@@ -1,27 +1,42 @@
-print("Weclome to Python calculator :D")
+print("Welcome to Python calculator :D")
 
-operation = str(input("Which operation are you going to use? Print:\n"
-      "'a' for 'Addition'\n's' for 'Subtraction'\n'd' for 'Division'\n'm' for 'Multiplication'\n"
-      "'e' for 'Exponent'\n'mod' for 'Modulus'\n'f' for 'Floor division'\n "))
+list_of_operations = ["a", 's', "d", "m", "e", "mod", "f"]
 
 
-first_number = float(input("Enter first integer of float number:\n"))
+while True:
+    operation = str(input("Which operation are you going to use? Print:\n"
+                          "'a' for 'Addition'\n's' for 'Subtraction'\n'd' for 'Division'\n'm' for 'Multiplication'\n"
+                          "'e' for 'Exponent'\n'mod' for 'Modulus'\n'f' for 'Floor division'\n "))
+    if operation.lower() in list_of_operations:
+        break
+    print("Please enter a correct operation!\n")
 
-second_number = float(input("Enter second integer of float number:\n"))
+while True:
+    first_number = input("Enter first integer:\n")
+    if first_number.isdigit():
+        break
+    print("Please enter a digit!\n")
+
+while True:
+    second_number = input("Enter second integer:\n")
+    if second_number.isdigit():
+        break
+    print("Please enter a digit!\n")
+
+first_number = int(first_number)
+second_number = int(second_number)
 
 if operation == "a":
-    print(f"Result {first_number + second_number}")
+    print(first_number + second_number)
 elif operation == "s":
-    print(f"Result {first_number - second_number}")
+    print(first_number - second_number)
 elif operation == "d":
-    print(f"Result {first_number / second_number}")
+    print(first_number / second_number)
 elif operation == "m":
-    print(f"Result {first_number * second_number}")
+    print(first_number * second_number)
 elif operation == "e":
-    print(f"Result {first_number ** second_number}")
+    print(first_number ** second_number)
 elif operation == "mod":
-    print(f"Result {first_number % second_number}")
+    print(first_number % second_number)
 elif operation == "f":
-    print(f"Result {first_number // second_number}")
-else:
-    print("You haven't chosen right option on the first step. Try again")
+    print(first_number // second_number)
