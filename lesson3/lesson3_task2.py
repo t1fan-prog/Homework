31449d635e3885ca923b818d00102e1ad0b9145e
t1fan <artem.t1fan@gmail.com>
2021-04-20 19:07:35 +0300

    Исправил проверку на ровно 10 символов

--- lesson3/lesson3_task2.py
+++ lesson3/lesson3_task2.py
@@ -2,7 +2,7 @@ while True:
     phone = input("Enter phone number. Please only use numbers without any symbols or characters "
                   "and country code (e.g. 0974356712). Also it must be not more than 10 digits:\n")
     if phone.isdecimal():
-        if len(phone) < 10:
+        if len(phone) == 10:
             print(f"Your phone {phone} is saved. Thank you! We will contact you soon.")
             break
-    print("Phone number can't contain a character or more than 10 digits. Try again!\n")
+    print("Phone number can't contain a character or be more/less than 10 digits. Try again!\n")
