f18715798b0c33864097450038592c23f79d0eaf
t1fan <artem.t1fan@gmail.com>
2021-04-26 22:22:42 +0300

    Доделал валидацию + dict comprehension в lesson6_task1.py

--- lesson6/lesson6_task1.py
+++ lesson6/lesson6_task1.py
@@ -2,12 +2,13 @@
 # and the number of occurrences as values.
 
 
-sentence = input("Type any sentence you like using only spaces without any symbols:\n")
+while True:
+    sentence = input("Type any sentence you like using only spaces without any symbols. "
+                     "Please use only one space between words and at the end of the sentence.\n")
+    sentence_list = sentence.split(" ")
+    if sentence_list.count("") <= 0:
+        break
+    print("You have added more that one space between words or at the and of the sentence. Try again.\n")
 
-sentence_list = sentence.split(" ")
-
-dic = {}
-for i in sentence_list:
-    dic[i] = sentence_list.count(i)
-
+dic = {key: sentence_list.count(key) for key in sentence_list}
 print(dic)
