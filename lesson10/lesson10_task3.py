cee575ce4bdf73bf266cec193f19237a5d637719
t1fan <artem.t1fan@gmail.com>
2021-06-02 14:50:56 +0300

    Homework lesson20
    Task1 - modules.py from lesson9_files_and_json/lesson9_task2/
    
    Task2 - lesson10_task3.py from lesson10

--- lesson10/lesson10_task3.py
+++ lesson10/lesson10_task3.py
@@ -1,18 +1,41 @@
+import random
+from typing import Union
+
+
 class TVController:
-    channels = None
-    current_channel_cls = None
 
-    def __init__(self, channels):
+    def __init__(self, channels: list):
         self.channels = channels
-        self.current_channel_cls = channels[0]
+        self.current_channel_cls: str = channels[0]
+        self.volume: int = 10
+        self.charge: Battery = Battery()
 
-    def first_channel(self):
-        print(f'"{self.channels[0]}"')
+    def volume_up(self, quantity: int):
+        self.charge.discharge()
+        total_volume = self.volume + quantity
+        if total_volume > 100:
+            self.volume = 100
+        else:
+            self.volume = total_volume
 
-    def last_channel(self):
-        print(f'"{self.channels[-1]}"')
+    def volume_down(self, quantity: int):
+        self.charge.discharge()
+        total_volume = self.volume - quantity
+        if total_volume < 0:
+            self.volume = 0
+        else:
+            self.volume = total_volume
 
-    def turn_channel(self, n):
+    def first_channel(self) -> str:
+        self.charge.discharge()
+        return f'"{self.channels[0]}"'
+
+    def last_channel(self) -> str:
+        self.charge.discharge()
+        return f'"{self.channels[-1]}"'
+
+    def turn_channel(self, n: int):
+        self.charge.discharge()
         try:
             self.current_channel_cls = self.channels[n - 1]
             print(f'"{self.current_channel_cls}"')
@@ -20,8 +43,9 @@ class TVController:
             print(f'There is no such channel. You have only {len(self.channels)} channels')
 
     def next_channel(self):
+        self.charge.discharge()
         try:
-            index_channel = self.channels.index(f'{self.current_channel_cls}')
+            index_channel: int = self.channels.index(f'{self.current_channel_cls}')
             self.current_channel_cls = self.channels[index_channel + 1]
             print(f'"{self.current_channel_cls}"')
         except IndexError:
@@ -29,8 +53,9 @@ class TVController:
             print(f'"{self.current_channel_cls}"')
 
     def previous_channel(self):
+        self.charge.discharge()
         try:
-            index_channel = self.channels.index(f'{self.current_channel_cls}')
+            index_channel: int = self.channels.index(f'{self.current_channel_cls}')
             self.current_channel_cls = self.channels[index_channel - 1]
             print(f'"{self.current_channel_cls}"')
         except IndexError:
@@ -38,15 +63,16 @@ class TVController:
             print(f'"{self.current_channel_cls}"')
 
     def current_channel(self):
+        self.charge.discharge()
         print(f'"{self.current_channel_cls}"')
 
-    def is_exist(self, n):
-        if type(n) == int:
+    def is_exist(self, n: Union[str, int]):
+        if isinstance(n, int):
             if n > len(self.channels):
                 print("No")
             else:
                 print("Yes")
-        else:
+        elif isinstance(n, str):
             self.channels = [x.lower() for x in self.channels]
             if n.lower() in self.channels:
                 print("Yes")
@@ -57,5 +83,47 @@ class TVController:
         print(self.channels)
 
 
+class Battery:  # type: ignore
+    def __init__(self):
+        self.charge_level = 100
+
+    def discharge(self):
+        new_charge_level = random.randint(self.charge_level - 10, self.charge_level - 1)
+        if new_charge_level > 0:
+            self.charge_level = new_charge_level
+        else:
+            raise Exception('Battery has run out of charge. Please put new one.')
+
+
 CHANNELS = ["BBS", "Discovery", "TV1000"]
 controller = TVController(CHANNELS)
+
+# controller.volume_down(10)
+# controller.volume_down(10)
+# controller.volume_down(10)
+# print(controller.volume)
+# print(controller.charge.charge_level)
+
+# import unittest
+#
+#
+# class TestController(unittest.TestCase):
+#
+#     def test_first_channel(self):
+#         some_TV = TVController(CHANNELS)
+#         self.assertEqual('BBS', some_TV.first_channel(), "Wrong channel")
+#
+#     # def test_isupper(self):
+#     #     self.assertTrue('FOO'.isupper())
+#     #     self.assertFalse('Foo'.isupper())
+#     #
+#     # def test_split(self):
+#     #     s = 'hello world'
+#     #     self.assertEqual(s.split(), ['hello', 'world'])
+#     #     # check that s.split fails when the separator is not a string
+#     #     with self.assertRaises(TypeError):
+#     #         s.split(2)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
--- lesson9_files_and_json/lesson9_task2/modules.py
+++ lesson9_files_and_json/lesson9_task2/modules.py
@@ -1,4 +1,6 @@
 import json
+from typing import Optional
+
 
 try:
     filename = 'phonebook.json'
@@ -8,7 +10,7 @@ except FileNotFoundError:
     print("There is no such a file in current directory")
 
 
-def formatting(file_dict_local, a):
+def formatting(file_dict_local, a: Optional[str]) -> dict:
     """Функция добавляет или заменяет полученную информацию от юзера к данным, полученным из файла
     Если параметр 'a' - None, то функция добавляет, если же есть значение - заменяет"""
     first_name = input("Enter first name:\n")
@@ -33,22 +35,23 @@ def formatting(file_dict_local, a):
     return file_dict_local
 
 
-def upload_new_info(local_filename, new_info):
+def upload_new_info(local_filename: str, new_info: dict) -> None:
     """Изменяет файл json"""
     with open(local_filename, 'w') as f:
         json.dump(new_info, f)
 
 
-def search_info(name, key):
+def search_info(name: str, key: str) -> Optional[dict]:
     """ Функция ищет совпадения заданных значений по ключу. Если совпадений нет, возвращает None.
         Параметры: name - что нужно искать, key - по какому ключу """
     for i in file_dict:
         x = file_dict[i].get(key).lower()
         if name.lower() == x:
             return file_dict[i]
+    return None
 
 
-def search_info_key(name, key):
+def search_info_key(name: str, key: str) -> Optional[str]:
     """ Функция ищет совпадения заданных значений по ключу. Если совпадений нет, возвращает None.
         Параметры: name - что нужно искать, key - по какому ключу
         Возвращает ключ словаря верхнего уровня"""
@@ -56,82 +59,83 @@ def search_info_key(name, key):
         x = file_dict[i].get(key).lower()
         if name.lower() == x:
             return i
+    return None
 
 
-def add_new_entries():  # Первый пункт меню!!
+def add_new_entries() -> None:  # Первый пункт меню!!
     """Функция добавления новых данных в json файл"""
     new_data = formatting(file_dict, None)
     upload_new_info(filename, new_data)
 
 
-def search_by_first_name():  # Второй пункт меню!!
+def search_by_first_name() -> str:  # Второй пункт меню!!
     name_choice = input("Enter name you want to search by:\n")
     result = search_info(name_choice, 'first_name')
     if result is not None:
-        print(f"Entry with name {name_choice.title()}: {result}\n")
+        return f"Entry with name {name_choice.title()}: {result}\n"
     else:
-        print("There is no entry with such a name\n")
+        return "There is no entry with such a name\n"
 
 
 def search_by_last_name():  # Третий пункт меню!!
     last_name_choice = input("Enter last name you want to search by:\n")
     result = search_info(last_name_choice, 'last_name')
     if result is not None:
-        print(f"Entry with last name {last_name_choice.title()}: {result}\n")
+        return f"Entry with last name {last_name_choice.title()}: {result}\n"
     else:
-        print("There is no entry with such a last name\n")
+        return "There is no entry with such a last name\n"
 
 
-def search_by_full_name():  # Четвёртый пункт меню!!
-    list_full_name = input("Enter full name you want to search by in format 'name surname'. For example 'Artem Shestak'"
-                           ":\n").split()
+def search_by_full_name() -> str:  # Четвёртый пункт меню!!
+    list_full_name: list = input("Enter full name you want to search by in format 'name surname'. "
+                                 "For example 'Artem Shestak':\n").split()
     name = list_full_name[0]
     last_name = list_full_name[1]
 
     result = [search_info(name, 'first_name'), search_info(last_name, 'last_name')]
     if None not in result:
-        print(f"Entry with full name {name.title()} {last_name.title()}: {result[0]}\n")
+        return f"Entry with full name {name.title()} {last_name.title()}: {result[0]}\n"
     else:
-        print("There is no entry with such a full name\n")
+        return "There is no entry with such a full name\n"
 
 
-def search_by_telephone_number():  # Пятый пункт меню!!
+def search_by_telephone_number() -> str:  # Пятый пункт меню!!
     phone = input("Enter the phone you want to search by:\n")
     result = search_info(phone, 'phone')
     if result is not None:
-        print(f"Entry with the phone {phone}: {result}\n")
+        return f"Entry with the phone {phone}: {result}\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
 
 
-def search_by_city_or_state():  # Шестой пункт меню!!
+def search_by_city_or_state() -> str:  # Шестой пункт меню!!
     city_or_state = input("Enter city or state you want to search by:\n")
     result = [search_info(city_or_state, 'city'), search_info(city_or_state, 'state')]
     if result[0] is not None:
-        print(f"Entry with city {city_or_state.title()}: {result[0]}\n")
+        return f"Entry with city {city_or_state.title()}: {result[0]}\n"
     elif result[1] is not None:
-        print(f"Entry with state {city_or_state.title()}: {result[1]}\n")
+        return f"Entry with state {city_or_state.title()}: {result[1]}\n"
     else:
-        print("There is no entry with such city of state\n")
+        return "There is no entry with such city of state\n"
 
 
-def delete_record_by_number():  # Седьмой пункт меню!!
+def delete_record_by_number() -> str:  # Седьмой пункт меню!!
     phone = input("Enter the phone number. Entry that contains this number will be deleted:\n")
     result = search_info_key(phone, 'phone')
     if result is not None:
         del file_dict[result]
         upload_new_info(filename, file_dict)
-        print(f"Entry with the {phone} has been deleted!\n")
+        return f"Entry with the {phone} has been deleted!\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
 
 
 def update_record_by_number():  # Восьмой пункт меню!!
     phone = input("Enter the phone number. Entry that contains this number will be updated:\n")
     result = search_info_key(phone, 'phone')
     if result is not None:
-        print(f"Current version of entry is:\n{file_dict[result]}\n\nHere you can update this entry.\n")
         formatting(file_dict, result)
         upload_new_info(filename, file_dict)
+        return f"Current version of entry is:\n{file_dict[result]}\n\nHere you can update this entry.\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
