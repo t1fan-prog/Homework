67bcbb774d5ec1d3418b2d65dee634050b67d07b
t1fan <artem.t1fan@gmail.com>
2021-06-03 14:14:54 +0300

    rework lesson18_task1.py and lesson17_task2.py

--- lesson17/lesson17_task2/lesson17_task2.py
+++ lesson17/lesson17_task2/lesson17_task2.py
@@ -69,51 +69,54 @@ def search_by_first_name():  # Второй пункт меню!!
     name_choice = input("Enter name you want to search by:\n")
     result = search_info(name_choice, 'first_name')
     if result is not None:
-        print(f"Entry with name {name_choice.title()}: {result}\n")
+        return f"Entry with name {name_choice.title()}: {result}\n"
     else:
-        print("There is no entry with such a name\n")
+        return "There is no entry with such a name\n"
 
 
 def search_by_last_name():  # Третий пункт меню!!
     last_name_choice = input("Enter last name you want to search by:\n")
     result = search_info(last_name_choice, 'last_name')
     if result is not None:
-        print(f"Entry with last name {last_name_choice.title()}: {result}\n")
+        return f"Entry with last name {last_name_choice.title()}: {result}\n"
     else:
-        print("There is no entry with such a last name\n")
+        return "There is no entry with such a last name\n"
 
 
 def search_by_full_name():  # Четвёртый пункт меню!!
     list_full_name = input("Enter full name you want to search by in format 'name surname'. For example 'Artem Shestak'"
                            ":\n").split()
-    name = list_full_name[0]
-    last_name = list_full_name[1]
+    if len(list_full_name) == 2:
+        name = list_full_name[0]
+        last_name = list_full_name[1]
+    else:
+        return "Enter 2 exactly 2 words\n"
 
     result = [search_info(name, 'first_name'), search_info(last_name, 'last_name')]
     if None not in result:
-        print(f"Entry with full name {name.title()} {last_name.title()}: {result[0]}\n")
+        return f"Entry with full name {name.title()} {last_name.title()}: {result[0]}\n"
     else:
-        print("There is no entry with such a full name\n")
+        return "There is no entry with such a full name\n"
 
 
 def search_by_telephone_number():  # Пятый пункт меню!!
     phone = input("Enter the phone you want to search by:\n")
     result = search_info(phone, 'phone')
     if result is not None:
-        print(f"Entry with the phone {phone}: {result}\n")
+        return f"Entry with the phone {phone}: {result}\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
 
 
 def search_by_city_or_state():  # Шестой пункт меню!!
     city_or_state = input("Enter city or state you want to search by:\n")
     result = [search_info(city_or_state, 'city'), search_info(city_or_state, 'state')]
     if result[0] is not None:
-        print(f"Entry with city {city_or_state.title()}: {result[0]}\n")
+        return f"Entry with city {city_or_state.title()}: {result[0]}\n"
     elif result[1] is not None:
-        print(f"Entry with state {city_or_state.title()}: {result[1]}\n")
+        return f"Entry with state {city_or_state.title()}: {result[1]}\n"
     else:
-        print("There is no entry with such city of state\n")
+        return "There is no entry with such city of state\n"
 
 
 def delete_record_by_number():  # Седьмой пункт меню!!
@@ -122,20 +125,20 @@ def delete_record_by_number():  # Седьмой пункт меню!!
     if result is not None:
         del file_dict[result]
         upload_new_info(filename, file_dict)
-        print(f"Entry with the {phone} has been deleted!\n")
+        return f"Entry with the {phone} has been deleted!\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
 
 
 def update_record_by_number():  # Восьмой пункт меню!!
     phone = input("Enter the phone number. Entry that contains this number will be updated:\n")
     result = search_info_key(phone, 'phone')
     if result is not None:
-        print(f"Current version of entry is:\n{file_dict[result]}\n\nHere you can update this entry.\n")
         formatting(file_dict, result)
         upload_new_info(filename, file_dict)
+        return f"Current version of entry is:\n{file_dict[result]}\n\nHere you can update this entry.\n"
     else:
-        print("There is no entry with such a phone\n")
+        return "There is no entry with such a phone\n"
 
 
 class TestPhonebook(unittest.TestCase):
@@ -146,13 +149,9 @@ class TestPhonebook(unittest.TestCase):
         "city": "Los Angeles",
         "state": "California"
     }
-    tady = {
-        "first_name": "Tady",
-        "last_name": "Talor",
-        "phone": "3809233245222",
-        "city": "Liberty City",
-        "state": "Test state"
-    }
+    with open("phonebook.json") as f:
+        l_dict = json.load(f)
+        tady = l_dict.get("person2")
 
     def test_search_info(self):
         self.assertEqual(search_info("test", "first_name"), None)
@@ -160,5 +159,19 @@ class TestPhonebook(unittest.TestCase):
         self.assertEqual(search_info("3809233245222", "phone"), self.tady)
 
     def test_search_by_first_name(self):
-        add_new_entries()
-        self.assertEqual(search_by_first_name(), self.tady, "No such first name")
+        print("Напиши какую-то фигню")
+        self.assertEqual("There is no entry with such a name\n", search_by_first_name())
+
+    def test_record_by_number(self):
+        """Ввести каку-то фигню, например 'dgdsfgds' """
+        print("Напиши что попало в следующих тестах")
+        self.assertEqual("There is no entry with such a phone\n", delete_record_by_number())
+        self.assertEqual("There is no entry with such a phone\n", update_record_by_number())
+        self.assertEqual("There is no entry with such city of state\n", search_by_city_or_state())
+        self.assertEqual("There is no entry with such a phone\n", search_by_telephone_number())
+        print("Здесь нужно ввести 2 рандомных слова")
+        self.assertEqual("There is no entry with such a full name\n", search_by_full_name())
+        print("Здесь нужно ввести либо 1 слово, либо больше двух")
+        self.assertEqual("Enter 2 exactly 2 words\n", search_by_full_name())
+        self.assertEqual("There is no entry with such a last name\n", search_by_last_name())
+        self.assertEqual("There is no entry with such a name\n", search_by_first_name())
